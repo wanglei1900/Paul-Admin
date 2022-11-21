546579104ae3831c3daa302a826826e196a46e06
wanglei1900 <819418426@qq.com>
2022-11-21 23:03:02 +0800

    完成动态菜单

--- src/layouts/components/Menu/SubMenu.vue
+++ src/layouts/components/Menu/SubMenu.vue
@@ -1,10 +1,39 @@
 <template>
-    {{ menuList }}
+    <template v-for="subItem in menuList">
+        <el-sub-menu v-if="subItem.children && subItem.children.length" :index="subItem.path">
+            <template #title>
+                <el-icon>
+                    <component :is="subItem.meta.icon"></component>
+                </el-icon>
+                <span>{{ subItem.meta.title }}</span>
+            </template>
+            <SubMenu :menuList="subItem.children" />
+        </el-sub-menu>
+        <el-menu-item v-else :index="subItem.path" @click="clickMenu(subItem)">
+            <template #title>
+                <el-icon>
+                    <component :is="subItem.meta.icon"></component>
+                </el-icon>
+                <span>{{ subItem.meta.title }}</span>
+            </template>
+        </el-menu-item>
+    </template>
 </template>
 
 <script setup lang='ts'>
-import { ref, reactive } from 'vue';
+import { useRouter } from 'vue-router';
 
-defineProps<{menuList:Menu.menuOptions[]}>()
+defineProps<{ menuList: Menu.menuOptions[] }>()
+
+const router = useRouter()
+
+const clickMenu = (subItem: Menu.menuOptions) => {
+    if (subItem.meta.isLink) window.open(subItem.meta.isLink, "_blank")
+    router.push(subItem.path)
+}
 
 </script>
+
+<style lang='scss' scoped>
+@import "./index.scss";
+</style>
